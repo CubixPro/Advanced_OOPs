8f6aa248c8f85dc53544eb59701fc2edb49a51b2
Srestha Kanjilal <72067381+sres1809@users.noreply.github.com>
2020-09-30 10:58:07 +0530

    Add files via upload

---
+++ Java/Assignment 1/Prog1_2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Prog1_2
+{
+	public static void main(String args[])
+	{
+		int n=0;
+		System.out.println("The input strings are :");
+		for(n=0;n<args.length;n++)
+		{
+			System.out.println(args[n]);
+		}
+		System.out.println("Number of strings= "+n);
+	}
+}
